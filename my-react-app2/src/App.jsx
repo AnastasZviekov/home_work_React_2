d6cd84d2cd2b9fcae5861e096d35d7b93ffc4769
AnastasZviekov <stas.zviekov@gmail.com>
2024-02-03 19:40:29 +0100

    add_components_from_html

---
+++ my-react-app2/src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import './App.css'
+import Header from "./components/Header/Header.jsx";
+import Form from "./components/Form/Form.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Header/>
+            <Form/>
+        </>
+    )
+}
+
+export default App
